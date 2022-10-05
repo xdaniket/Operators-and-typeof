c5f09b6c5c47ced5049721a43dda127cafd95e2c
Aniket <aniketthakur8808@gmail.com>
2022-10-05 22:14:05 +0530

    Practice in all operator and typeof

---
+++ operator-practice.js
@@ -0,0 +1,74 @@
+//                                    Arthmatic operators..
+
+// let a = 10;
+// let b = 20;
+// let d = a + b; //        30
+// d = a - b; //           -10
+// d = a * b; //            200
+// d = a / b; //            0.5
+// d = a % b; //            10
+// d = a ** b; //           100....
+// d = Math.pow(a, b); //   100....
+
+// console.log(d);
+
+//                                    Assignment Operator
+
+let annu = "thakur";
+let aannu;
+aannu = annu;
+// console.log(aannu);
+
+let a = 10;
+let b = 100;
+c = a += b;
+// console.log(c); //        110
+
+let d = 100;
+let e = 100;
+d += e;
+// console.log(d); //        200
+
+let f = 12;
+let g = 20;
+g += f;
+console.log(g); //           32
+
+let i = 120;
+let j = 20;
+i /= j;
+console.log(i); //           6
+
+let k = 120;
+let l = 20;
+k %= l;
+console.log(k); //           0
+
+//                                     Comparison Operator
+
+//
+console.log(1 !== 1); //    false
+console.log(1 !== "1"); //  true
+console.log(1 != "1"); //   false
+
+//
+
+let o = 120; //     let o = 120;    //  let o = 120;     //   let o = 120;
+let p = 1120; //    let p = "120"; //   let p = "1201"  //    let p = 1120;
+//
+
+//
+let q = o !== p;
+console.log(q); //                  first check value  so true
+//
+if (o === p) {
+  console.log("true");
+} else {
+  console.log("false"); //          false
+}
+//
+
+let m = 10;
+let n = 20;
+let sum = m !== n; //   doubt
+console.log(sum);
---
+++ theory.js
@@ -0,0 +1,134 @@
+//==================      OPERATORS       ====================.
+//
+//   Operators :
+//              An operators in a programming language is a symbol like (addition,sub,mul,div)
+//              that tells the compilar to perform specific mathematical,relational or
+//              logical operations and produce final result.
+//
+//   There are five types of operators in JavaScript :
+//   1. Arithmetic Oper  2. Assignment Oper  3. Comparison Oper  4. Logical Oper  5. Other Oper.
+//
+//
+//   1. Arithmetic Operator :
+//                          Arthmetic Operator are used to perform arithmetic calculations.
+//                          Exa : ( + , - , * , / , % , ++ , -- , ** )
+//
+//                   Note: Power like  2**3 = 2*2*2 = 8
+//                   Math.pow(2,3) = 8
+//
+//
+//   2. Assignment Operator :
+//                          Assignment Operator are used to assign a value to the variable.
+//                          Exa : ( = Assignment operator ) ( += Addition assgnmt opertr )
+//                                ( -= subs assgnmt opertr ) ( *= Mult assgnmt opertr )
+//                    Same all    ( /= , %= , **= )
+//
+//
+//   3. Comparison Operator :
+//                           Comparison operator are used to compare two values and it return
+//                           a boolean value, either true or false.
+
+//     Exa : ( " == "  .  if the operands are equal. ) ( operands mean value. )
+//           ( " === " .  if the operands and their datatype both are equal. )
+
+//           ( " != "  .  if the operands are not equal.)
+//           ( " !== " .  if the operands and their datatype both are not equal.)
+//
+//           ( " > "   .  if left operands is greater than the right operand.)
+//           ( " >= "  .  if left operands is greater than or equal to the right operand.)
+//
+//           ( " < "   .  if right operands is greater than the left operand.)
+//           ( " <= "  .  if right operands is greater than or equal to the left operand.)
+//
+//
+//   4. Logical Operators :
+//                       Logical operators are used to determine logic b/w variables or values.
+//
+//     There are three types of logical operators :
+//     A. logical && (AND) operators  B. logical || (OR) operators  C. logical !(NOT) operators
+//
+//     A. logical AND operators :
+//                             If both the operands are true than the condition will bcm true.
+//
+//     B. logical OR operators :
+//                             If any of the two operands are true than the condition will bcm true.
+//
+//     C. logical NOT operators :
+//                             If the condition is true so the logical NOT oprtr will make it false
+//
+//  5. Other Operator =
+//         1. ( , )
+//                         let a = (1,2,3)        (written always last value) = (3).
+//                         let a = (1,2,3,"annu") (written always last value) = ("annu").
+//
+//         2. ( typeOf ) = ("written a datatype in string")
+//                                                          let a = 23;
+//                                                          console.log(typeof a) = "number"
+//
+//         3. (condition ternary operator)
+//                                          let a = 130;
+//                                          let b = 40;
+//                                          let c = a > b ? "true" : "false";
+//                                          console.log(c);
+//
+//
+//         4. (in)
+//
+//         5. (instanceof)
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//  ---------------------------    Extra Question    --------------------------
+
+// let a = 1 && 2;
+// console.log(a);
+
+// let b = 1 || 2;
+// console.log(b);
+
+// let a = [10];
+// let b = [10];
+// console.log(a[0] == b[0]);
+
+// let a = 23;
+// let b = 23;
+// console.log(a == b);
+
+let a = 10;
+let c = a++;
+console.log(c);
+console.log(a);
+
+// let a = 2;
+// a++;
+// console.log(a);
+
+// let c = 20;
+// if (!c == 0) {
+//   console.log("guy");
+// }
+
+// let a = 30;
+// let b = 30;
+// let c = !a == b;
+// console.log(c);
+
+// let a = 30;
+// let b = 30;
+// let c = 10;
+
+// if (!a != c) {
+//   console.log("h");
+// }
+
+// if (!a == b) {
+//   console.log("Equal");
+// }
